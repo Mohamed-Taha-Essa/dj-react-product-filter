1e2a9ef25d6ecf7d789eb838eacc7b29b0a70a69
Mohamed Abdallah Essa <www.taha.166@gmail.com>
2024-08-07 17:40:17 +0200

    add image field & add media & static setting

--- project/products/models.py
+++ project/products/models.py
@@ -15,6 +15,8 @@ class Product(models.Model):
     stock = models.PositiveIntegerField()
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
-
+    image =models.ImageField(upload_to='product', height_field=None, width_field=None, max_length=None)
     def __str__(self):
         return self.name
+
+#PS1='\[\033]0;$TITLEPREFIX:$PWD\007\033[32m\]\u@\h \[\033[35m\w\033[32m\]>\[\033[0m\] '
--- project/project/urls.py
+++ project/project/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.urls import path ,include
 
 urlpatterns = [
@@ -22,3 +25,6 @@ urlpatterns = [
     path('api/', include("products.urls")),
 
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
