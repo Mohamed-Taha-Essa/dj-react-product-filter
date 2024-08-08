99eb6a0220a90e39f234e254f391903ad5e78f2c
Mohamed Abdallah Essa <www.taha.166@gmail.com>
2024-08-08 13:46:39 +0200

    add custom pagination for products

--- project/products/api.py
+++ project/products/api.py
@@ -4,13 +4,14 @@ from django_filters.rest_framework import DjangoFilterBackend
 from .serializers import ProductSerialzers ,CategorySerialzers
 from .models import Product ,Category
 from .filters import ProductFilter
-
+from .pagination import ProductPagination
 
 class ProductList(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerialzers
     filter_backends=[DjangoFilterBackend]
     filterset_class = ProductFilter
+    pagination_class =ProductPagination
 
 
 class CategoryList(generics.ListAPIView):
