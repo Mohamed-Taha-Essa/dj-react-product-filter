5704757dc2db31af8a29633d6f74b24993a55056
Mohamed Abdallah Essa <www.taha.166@gmail.com>
2024-08-14 18:17:52 +0200

    update requirements.txt

--- product-filter/src/components/ProductList.jsx
+++ product-filter/src/components/ProductList.jsx
@@ -22,21 +22,27 @@ const ProductList =()=>{
         <div>
             <h2>ProductList</h2>
             <div className="row">
-            {products.map((product ,index) =>(
-                <div key={index} className="col-4">
-                    <div  className="card">
-                        <img src={product.image} className="card-img-top"/>
-                        <div className="card-body">
-                            <h5 className="card-title">{product.name}</h5>
-                            <p>{product.price}</p>
-                            <p className="card-text">{product.description}</p>
-                            <a href="#" className="btn btn-primary">Add To Cart</a>
+                {products.map((product ,index) =>(
+                    <div key={index} className="col-4">
+                        <div  className="card">
+                            <img src={product.image} className="card-img-top"/>
+                            <div className="card-body">
+                                <h5 className="card-title">{product.name}</h5>
+                                <p>{product.price}</p>
+                                <p className="card-text">{product.description}</p>
+                                <a href="#" className="btn btn-primary">Add To Cart</a>
+                            </div>
                         </div>
-                     </div>
-                </div>
-                ))}
+                    </div>
+                    ))}
             
             </div>
+            <div className="mt-5">
+                <button className="btn btn-primary">prev</button>
+                <button className="btn btn-primary">next</button>
+
+            </div>
+            
         </div> 
 
         );
